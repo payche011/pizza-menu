5e13612d515515040104f6b576cbaf09fb32b653
Pavle Marinkovic <pavlemarinkovic@pavles-mbp.localdomain>
2023-07-24 12:10:07 +0200

    pizza-menu

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}. We're currently open
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1 className="header">Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+export default Header;
